85d1265f506c3a6407bafa9e35bbc85be810ea6c
gbyat <gaby@gby.at>
2025-11-27 15:25:05 +0100

    Release v0.1.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.9] - 2025-11-27
+
+- Version update
+
+
 ## [0.1.8] - 2025-11-27
 
 - Remove temporary files from repository and add to .gitignore
@@ -154,3 +159,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 [0.1.6]: https://github.com/gbyat/we-custom-fields-block/releases/tag/v0.1.6
 [0.1.7]: https://github.com/gbyat/we-custom-fields-block/releases/tag/v0.1.7
 [0.1.8]: https://github.com/gbyat/we-custom-fields-block/releases/tag/v0.1.8
+[0.1.9]: https://github.com/gbyat/we-custom-fields-block/releases/tag/v0.1.9
--- src/render.php
+++ src/render.php
@@ -1,65 +1,67 @@
 <?php
 
 /**
- * Block render callback
+ * Block render template
  *
- * @param array    $attributes Block attributes.
- * @param string   $content    Block content.
- * @param WP_Block $block      Block instance.
- * @return string Rendered block HTML.
+ * @var array    $attributes Block attributes.
+ * @var string   $content    Block content.
+ * @var WP_Block $block      Block instance.
  */
-function cfb_render_block($attributes, $content, $block)
-{
-    // Get block attributes
-    $field_key = isset($attributes['fieldKey']) ? $attributes['fieldKey'] : '';
-    $display_type = isset($attributes['displayType']) ? $attributes['displayType'] : 'paragraph';
-    $heading_level = isset($attributes['headingLevel']) ? intval($attributes['headingLevel']) : 2;
 
-    // Return early if no field key
-    if (empty($field_key)) {
-        return '';
-    }
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
 
+// Get block attributes
+$field_key = isset($attributes['fieldKey']) ? $attributes['fieldKey'] : '';
+$display_type = isset($attributes['displayType']) ? $attributes['displayType'] : 'paragraph';
+$heading_level = isset($attributes['headingLevel']) ? intval($attributes['headingLevel']) : 2;
+
+// Only render if we have a field key and a value
+if (!empty($field_key)) {
     // Get field value
     $field_value = get_post_meta(get_the_ID(), $field_key, true);
 
-    // Return early if no value
-    if (empty($field_value)) {
-        return '';
-    }
+    // Only render if we have a value (not empty, not false)
+    if ($field_value !== false && $field_value !== '') {
+        // Convert value to string for display
+        $display_value = (string) $field_value;
 
-    // Get block wrapper attributes (includes all theme styles)
-    $wrapper_attributes = get_block_wrapper_attributes(array(
-        'class' => 'cfb-block'
-    ));
+        // Get block wrapper attributes (includes all theme styles)
+        $wrapper_attributes = get_block_wrapper_attributes(array(
+            'class' => 'cfb-block'
+        ));
 
-    // Render based on display type
-    switch ($display_type) {
-        case 'heading':
-            // Validate heading level (1-6)
-            $heading_level = max(1, min(6, $heading_level));
-            $tag = 'h' . $heading_level;
+        // Render based on display type
+        switch ($display_type) {
+            case 'heading':
+                // Validate heading level (1-6)
+                $heading_level = max(1, min(6, $heading_level));
+                $tag = 'h' . $heading_level;
+?>
+                <<?php echo esc_attr($tag); ?> <?php echo $wrapper_attributes; ?>>
+                    <?php echo esc_html($display_value); ?>
+                </<?php echo esc_attr($tag); ?>>
+            <?php
+                break;
 
-            return sprintf(
-                '<%1$s %2$s>%3$s</%1$s>',
-                $tag,
-                $wrapper_attributes,
-                esc_html($field_value)
-            );
+            case 'div':
+            ?>
+                <div <?php echo $wrapper_attributes; ?>>
+                    <?php echo esc_html($display_value); ?>
+                </div>
+            <?php
+                break;
 
-        case 'div':
-            return sprintf(
-                '<div %s>%s</div>',
-                $wrapper_attributes,
-                esc_html($field_value)
-            );
-
-        case 'paragraph':
-        default:
-            return sprintf(
-                '<p %s>%s</p>',
-                $wrapper_attributes,
-                esc_html($field_value)
-            );
+            case 'paragraph':
+            default:
+            ?>
+                <p <?php echo $wrapper_attributes; ?>>
+                    <?php echo esc_html($display_value); ?>
+                </p>
+<?php
+                break;
+        }
     }
 }
